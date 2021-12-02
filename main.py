ad9c03c158ee355bd8cbbad38f9e3a403b9aed08
Julian Reh <julian.reh@infomotion.de>
2021-12-02 22:29:05 +0100

    Refactor Code

--- main.py
+++ main.py
@@ -7,49 +7,39 @@ import src.PandasETLHelpers.SCDHelpers as scd
 
 key_columns = ['Lastname','Firstname']
 current_path = './data/current/current.parquet'
-first_run_delta_path = './data/grades_delta_old.csv'
-second_run_delta_path = './data/grades_delta_new.csv'
+    
+delta_sql_string = 'select a.* from new_data_df a LEFT JOIN current_df b ON a.KEY_HASH = b.KEY_HASH AND a.RECORD_HASH = b.RECORD_HASH WHERE b.KEY_HASH IS NULL'
 
-first_run_full_path = './data/grades_full_old.csv'
-second_run_full_path = './data/grades_full_new.csv'
+def meta_column_delta(file_path):
+    currents = mch.create_currents()
+    new_data_df = pd.read_csv(file_path)
+    new_data_df = mch.add_meta_columns(new_data_df,currents,key_columns)
 
-def meta_columns_delta(first_run_file_path, second_run_file_path):
+    current_df = scd.read_parquet_df(current_path)
+    if current_df is None:
+        current_df = pd.DataFrame(columns=new_data_df.columns)
+    current_df_delta = ps.sqldf(delta_sql_string)
+    current_df = pd.concat([current_df,current_df_delta])
+    current_df.to_parquet(current_path,partition_cols=key_columns,engine='fastparquet')
+
+def simulate_runs(first_run_file_path, second_run_file_path):
     if os.path.exists(current_path):
         shutil.rmtree(current_path)
-    first_run_data_df = pd.read_csv(first_run_file_path)
-    first_run_ts = '2020-03-20 13:00:00'
-    first_run_currents = mch.create_currents(first_run_ts)
-    first_run_data_df = mch.add_meta_columns(first_run_data_df,first_run_currents,key_columns)
-
-    first_run_current_df = scd.read_parquet_df(current_path)
-    if first_run_current_df is None:
-        first_run_current_df = pd.DataFrame(columns=first_run_data_df.columns)
-    current_df = ps.sqldf('select a.* from first_run_data_df a LEFT JOIN first_run_current_df b ON a.KEY_HASH = b.KEY_HASH AND a.RECORD_HASH = b.RECORD_HASH WHERE b.KEY_HASH IS NULL')
-    current_df = pd.concat([current_df,first_run_current_df])
-    current_df.to_parquet(current_path,partition_cols=key_columns,engine='fastparquet')
-    
-    second_run_data_df = pd.read_csv(second_run_file_path)
-    second_run_ts = '2021-04-15 11:12:30'
-    second_run_currents = mch.create_currents(second_run_ts)
-    second_run_data_df = mch.add_meta_columns(second_run_data_df,second_run_currents,key_columns)
-
-    second_run_current_df = scd.read_parquet_df(current_path)
-    if second_run_current_df is None:
-        second_run_current_df = pd.DataFrame(columns=second_run_data_df.columns)
-    current_df = ps.sqldf('select a.* from second_run_data_df a LEFT JOIN second_run_current_df b ON a.KEY_HASH = b.KEY_HASH AND a.RECORD_HASH = b.RECORD_HASH WHERE b.KEY_HASH IS NULL')
-    current_df = pd.concat([current_df,second_run_current_df])
-    current_df.to_parquet(current_path,partition_cols=key_columns,engine='fastparquet')
+    meta_column_delta(first_run_file_path)
+    meta_column_delta(second_run_file_path)
 
     final_df = scd.read_parquet_df(current_path)
     return final_df
 
-    
-    
-
-
 if __name__ == '__main__':
-    delta_final_df = meta_columns_delta(first_run_delta_path,second_run_delta_path)
-    full_final_df = meta_columns_delta(first_run_full_path,second_run_full_path)
+
+    first_run_delta_path = './data/grades_delta_old.csv'
+    second_run_delta_path = './data/grades_delta_new.csv'
+    delta_final_df = simulate_runs(first_run_delta_path, second_run_delta_path)
     print(delta_final_df)
+
+    
+    first_run_full_path = './data/grades_full_old.csv'
+    second_run_full_path = './data/grades_full_new.csv'
+    full_final_df = simulate_runs(first_run_full_path, second_run_full_path)
     print(full_final_df)
-    print(delta_final_df.equals(full_final_df))
